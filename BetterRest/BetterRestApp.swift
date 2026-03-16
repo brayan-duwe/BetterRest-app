547d06d2694454535a39ce3aed2832485e24b085
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-03-16 19:38:55 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Brayan Duwe on 16/03/26.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
